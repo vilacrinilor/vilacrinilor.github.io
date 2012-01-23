1fa7c7ad63553ce434b0cae55a717a5a1fcee926
Alex Palcuie <alex.palcuie@gmail.com>
2012-01-23 21:38:50 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,117 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Vila, Crinilor, Calimanesti, Caciulata" />
+    <meta name="description" content="Vila Crinilor - Calimanesti Caciulata" />
+	<title>Vila Crinilor - Calimanesti Caciulata</title>
+    <link rel="stylesheet" type="text/css" media="screen, projection" href="stylesheets/screen.css" /> 
+    <!--[if IE]>
+        <link rel="stylesheet" href="stylesheets/ie.css" type="text/css" media="screen, projection" />
+    <![endif]--> 
+    <link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css' />
+    <link rel="stylesheet" href="stylesheets/vilacrinilor.css" type="text/css" media="screen, projection" />
+    <!--[if IE]>
+        <link rel="stylesheet" href="stylesheets/vilacrinilorie.css" type="text/css" media="screen, projection" />
+    <![endif]--> 
+    <script type="text/javascript" src="vilacrinilor.js"></script>
+</head>
+<body>
+	<div class="container">
+    	<div id="header" class="span-24">
+            	<p><img src="images/header.jpg" width="950" height="170" alt="header" /></p>
+        </div>
+    </div>
+    <div id="subheader">
+    	<h1>Bine ati venit la Vila Crinilor *** Calimanesti - Caciulata</h1>
+    </div>
+    <div class="container" id="wrap">
+  		<div class="span-16" id="main">
+	<div id="oferta">
+		<p id="oferta_title" style="font-size:170%;">
+			<span style="font-size:160%">Oferta Revelion 2012</span><br />30 decembrie 2011 - 02 ianuarie 2012
+		</p>
+		<ul style="font-size:150%;margin-bottom:5px">
+			<li>Cazare = 390 lei / camera dubla / sejur</li>
+			<li>Cazare = 595 lei / apartament / sejur</li>
+		</ul>
+<!--
+		<p style="margin-top:0px;font-size:135%;margin-bottom:0px;">
+			Se acorda discount de pana la <b>11%</b> (toata vila) pentru inchirierea unui numar mai mare de camere.	
+		</p>
+		<p style="margin-top:0px;font-size:135%;color:red;">
+			Bonus!!! O noapte cazare gratuita la inceputul sau sfarsitul sejurului.
+		</p>
+-->
+		<hr>
+	</div>            
+	<div id="content">
+            	<h2>Vila Crinilor reprezinta o oaza de relaxare, avand o capacitate de 32 de locuri, cu o curte de aproximativ 1500 de metri patrati. De asemenea va ofera posibilitatea de a gati, punandu-va la dispozitie o bucatarie complet utilata, o sala de mese de aproximativ 60 persoane si doua gratare.</h2>
+             <div id="double_list">
+                <div class="span-7 colborder" id="date_contact">
+                <p class="content_title">Date Contact</p>
+                <ul>
+                	<li>Str. Calea lui Traian, Nr. 485</li>
+                    <li>Calimanesti-Caciulata</li>
+                    <li>Telefon/Fax: 0250750188</li>
+              		<li>Mobil: 0728703703</li>
+                    <li><a href="mailto:vilacrinilor@yahoo.com">vilacrinilor@yahoo.com</a></li>
+                    <li><a href="http://www.vilacrinilor.ro">www.vilacrinilor.ro</a></li>
+                </ul>
+                </div>
+                <div class="span-7 last" id="descriere">
+                <p class="content_title">Acces</p>
+         		<ul class="lista_bullet" id="lista_acces">
+                	<li>Sala de mese 60 persoane</li>
+                  	<li>Bucatarie complet utilata</li>
+                    <li>Doua gratare</li>
+                    <li>Parcare</li>
+                    <li>Automat Cafea</li>
+                    <li>Biliard</li>
+                    <li>Tenis de masa</li>
+                </ul>
+             </div>
+                </div>
+                <p class="content_title">Descriere</p>
+  				<ul class="lista_bullet" id="lista_descriere">
+                	<li>un apartament = pat matrimonial + canapea extensibila</li>
+                    <li>7 camere duble cu pat matrimonial</li>
+                    <li>8 camere duble cu paturi single</li>
+                </ul>
+              <p id="descriere_camere">Toate camerele au bai proprii, TV cu cablu, frigider si aer conditionat.</p>
+            </div>
+		</div>
+        <div class="span-8 last" id="sidebar">
+        	<div id="navigation">
+            	<ul>
+					<li><a href="index.html" class="active">Acasa</a></li>
+					<li><a href="album/index.html">Fotografii</a></li>
+					<li><a href="where.html">Cum ajung?</a></li>
+                    <li><a href="parteneri.html">Parteneri</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+            </div>
+            
+            <!--[if IE]>
+			<iframe src="http://www.facebook.com/plugins/likebox.php?id=141360355893549&amp;width=292&amp;connections=10&amp;stream=true&amp;header=true&amp;height=587" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:587px;" allowTransparency="true"></iframe>
+			<![endif]-->
+            
+			<!--[if !IE]>  <-->
+			<object data="http://www.facebook.com/plugins/likebox.php?id=141360355893549&amp;width=292&amp;connections=10&amp;stream=true&amp;header=true&amp;height=587" width="292" height="587"></object>
+			<!--> <![endif]--> 
+            
+            <div id="links">
+            	<ul>
+                	<li><a href="http://www.turismvirtual.ro/Vila-Crinilor-674.html"><img src="http://www.turismvirtual.ro/logo.gif" alt="Cazare pensiuni hoteluri Calimanesti - Caciulata" width="88" height="31" border="0" /></a></li>
+                    <li><a href="http://www.cazarionline.ro/cazare/Valcea/Vila/537-Vila-Crinilor.html"><img src="http://www.cazarionline.ro/images/mlogo.gif?id=537" alt="Cazari Online Vila Crinilor" width="88" height="31" /></a></li>
+                    <li><script type="text/javascript" src="http://www.infopensiuni.ro/cod/cod.js"></script><a href="http://www.infopensiuni.ro/" target="_blank"><img src="http://static.infoturism.ro/interface/cod_parteneri/cod_link_parteneri_infopensiuni.gif" style="border: none;" alt="Cazare vile si pensiuni" /></a><br /></li>
+                <ul>
+            </div>
+        </div>
+	</div>
+    <div id="footer"> 
+        <p>Realizat de catre <a href="http://palcu.blogspot.com">Alexandru Palcuie</a>. Copyright &copy; 2011
+	</p>
+    </div> 
+</body>
+</html>
